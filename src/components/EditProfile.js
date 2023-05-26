68ee6aa3265f49aad3646414c348a7bcfa0a0ec9
MichaelShawUK <MichaelShawUK@gmail.com>
2023-05-26 18:50:37 +0100

    Update user only if details differ from database document

--- src/components/EditProfile.js
+++ src/components/EditProfile.js
@@ -2,7 +2,10 @@ import StyledEditProfile from "../styles/StyledEditProfile";
 import StyledButton from "../styles/StyledButton";
 import Loading from "./Loading";
 import uploadImage from "../utils/cloudinary";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useFetcher } from "react-router-dom";
+import axios from "axios";
+import { database } from "../data/constants";
 
 const EditProfile = () => {
   const firstName = localStorage.getItem("firstName");
@@ -11,59 +14,107 @@ const EditProfile = () => {
   const [imageUrl, setImageUrl] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
+  let fetcher = useFetcher();
+
+  useEffect(() => {
+    async function updateUser(data) {
+      await axios({
+        method: "POST",
+        url: `${database}/edit`,
+        headers: { Authorization: `Bearer ${localStorage.token}` },
+        data,
+      });
+    }
+
+    function isEdited(formData) {
+      if (formData.firstName !== localStorage.getItem("firstName")) {
+        return true;
+      }
+      if (formData.lastName !== localStorage.getItem("lastName")) {
+        return true;
+      }
+      if (formData.imageUrl) {
+        return true;
+      }
+      return false;
+    }
+
+    if (fetcher.formData) {
+      const formData = Object.fromEntries(fetcher.formData);
+      if (isEdited(formData)) {
+        updateUser(formData);
+      }
+    }
+  }, [fetcher]);
+
   return (
     <StyledEditProfile>
-      <h3>Edit Profile</h3>
-      <label htmlFor="firstName">First Name</label>
-      <input
-        type="text"
-        placeholder={firstName}
-        name="firstName"
-        id="firstName"
-        autoFocus
-        defaultValue={firstName}
-      ></input>
-      <label htmlFor="lastName">Last Name</label>
-      <input
-        type="text"
-        placeholder={lastName}
-        defaultValue={lastName}
-        name="lastName"
-        id="lastName"
-      ></input>
-      <label htmlFor="avatar">Avatar</label>
-      {imageUrl ? (
-        <>
-          <img src={imageUrl} alt="" className="avatarImage"></img>
-          <button
-            className="resetButton"
-            type="reset"
-            onClick={() => setImageUrl("")}
-          >
-            Clear Avatar
-          </button>
-        </>
-      ) : (
+      <fetcher.Form>
+        <h3>Edit Profile</h3>
+        <label htmlFor="firstName">First Name</label>
         <input
-          type="file"
-          accept="image/*"
-          id="avatar"
-          name="avatar"
-          onChange={(e) => {
-            setIsLoading(true);
-            const reader = new FileReader();
-            reader.readAsDataURL(e.target.files[0]);
-            reader.onerror = () => console.log("Failed to convert to base64");
-            reader.onload = async () => {
-              const { data } = await uploadImage(reader.result);
-              setImageUrl(data.secure_url);
-              setIsLoading(false);
-            };
-          }}
+          type="text"
+          placeholder={firstName}
+          name="firstName"
+          id="firstName"
+          autoFocus
+          defaultValue={firstName}
         ></input>
-      )}
-      <input hidden type="text" name="imageUrl" value={imageUrl}></input>
-      {isLoading ? <Loading /> : <StyledButton>Update</StyledButton>}
+        <label htmlFor="lastName">Last Name</label>
+        <input
+          type="text"
+          placeholder={lastName}
+          defaultValue={lastName}
+          name="lastName"
+          id="lastName"
+        ></input>
+        <label htmlFor="avatar">Avatar</label>
+        {imageUrl ? (
+          <>
+            <img src={imageUrl} alt="" className="avatarImage"></img>
+            <button
+              className="resetButton"
+              type="reset"
+              onClick={() => setImageUrl("")}
+            >
+              Clear Avatar
+            </button>
+          </>
+        ) : (
+          <input
+            type="file"
+            accept="image/*"
+            id="avatar"
+            name="avatar"
+            onChange={(e) => {
+              setIsLoading(true);
+              const reader = new FileReader();
+              reader.readAsDataURL(e.target.files[0]);
+              reader.onerror = () => console.log("Failed to convert to base64");
+              reader.onload = async () => {
+                const { data } = await uploadImage(reader.result);
+                setImageUrl(data.secure_url);
+                setIsLoading(false);
+              };
+            }}
+          ></input>
+        )}
+        <input
+          hidden
+          readOnly
+          type="text"
+          name="imageUrl"
+          value={imageUrl}
+        ></input>
+        <input
+          hidden
+          readOnly
+          type="text"
+          name="userId"
+          value={localStorage.getItem("userId")}
+        ></input>
+        {isLoading ? <Loading /> : <StyledButton>Update</StyledButton>}
+      </fetcher.Form>
     </StyledEditProfile>
   );
 };
--- src/styles/StyledEditProfile.js
+++ src/styles/StyledEditProfile.js
@@ -1,58 +1,60 @@
 import { styled } from "styled-components";
 
-const StyledEditProfile = styled.form`
-  max-width: 800px;
-  margin: 0 auto;
-  display: grid;
-  gap: 0.5rem;
-  background-color: ${(props) => props.theme.secondary};
-  padding: 1rem 2rem;
-  border-radius: 4px;
-  box-shadow: 0px 0px 4px ${(props) => props.theme.shadow};
-
-  .resetButton {
-    justify-self: flex-start;
-  }
-
-  .avatarImage {
-    height: 60px;
-    width: 60px;
-    object-fit: cover;
-    border-radius: 6px;
-  }
-
-  > input,
-  h3 {
-    margin-bottom: 0.5rem;
-  }
-
-  > input {
-    background-color: ${(props) => props.theme.bg};
-    border: none;
-    padding: 12px;
-    border-radius: 6px;
-    color: inherit;
-
-    &:focus-visible {
-      outline: 1px solid ${(props) => props.theme.fg};
-    }
-  }
-
-  > input[type="file"] {
+const StyledEditProfile = styled.div`
+  form {
+    max-width: 800px;
+    margin: 0 auto;
+    display: grid;
+    gap: 0.5rem;
     background-color: ${(props) => props.theme.secondary};
-    cursor: pointer;
-    padding-left: 0;
-  }
+    padding: 1rem 2rem;
+    border-radius: 4px;
+    box-shadow: 0px 0px 4px ${(props) => props.theme.shadow};
 
-  > button {
-    min-width: 100px;
-    width: max-content;
-    justify-self: center;
-  }
+    .resetButton {
+      justify-self: flex-start;
+    }
 
-  @media (max-width: 700px) {
-    padding: 1rem 1rem;
-    border-radius: 0;
+    .avatarImage {
+      height: 60px;
+      width: 60px;
+      object-fit: cover;
+      border-radius: 6px;
+    }
+
+    > input,
+    h3 {
+      margin-bottom: 0.5rem;
+    }
+
+    > input {
+      background-color: ${(props) => props.theme.bg};
+      border: none;
+      padding: 12px;
+      border-radius: 6px;
+      color: inherit;
+
+      &:focus-visible {
+        outline: 1px solid ${(props) => props.theme.fg};
+      }
+    }
+
+    > input[type="file"] {
+      background-color: ${(props) => props.theme.secondary};
+      cursor: pointer;
+      padding-left: 0;
+    }
+
+    > button {
+      min-width: 100px;
+      width: max-content;
+      justify-self: center;
+    }
+
+    @media (max-width: 700px) {
+      padding: 1rem 1rem;
+      border-radius: 0;
+    }
   }
 `;
 
