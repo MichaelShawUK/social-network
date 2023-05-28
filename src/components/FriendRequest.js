6071b66949c5ab0a9f8d42402d491f9fd6267068
MichaelShawUK <MichaelShawUK@gmail.com>
2023-05-28 12:31:52 +0100

    Conditionally render FriendRequest depending on friendship status

--- src/components/FriendRequest.js
+++ src/components/FriendRequest.js
@@ -2,12 +2,19 @@ import StyledButton from "../styles/StyledButton";
 import StyledFriendRequest from "../styles/StyledFriendRequest";
 
 const FriendRequest = ({ user }) => {
+  const friendIds = user.friends.map((friend) => friend._id);
+  const isFriend = friendIds.includes(localStorage.getItem("userId"));
+
   return (
     <StyledFriendRequest>
       <img src={user.avatar} alt=""></img>
       <div className="name">{`${user.firstName} ${user.lastName}`}</div>
       <form>
-        <StyledButton>Friend Request</StyledButton>
+        {isFriend ? (
+          <StyledButton>Unfriend</StyledButton>
+        ) : (
+          <StyledButton>Friend Request</StyledButton>
+        )}
       </form>
     </StyledFriendRequest>
   );
