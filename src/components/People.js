f0ebb5891b3ec4fbf1e781ecd204276959ab6f61
MichaelShawUK <MichaelShawUK@gmail.com>
2023-06-03 18:34:05 +0100

    Display loading spinner for People page load

--- src/components/People.js
+++ src/components/People.js
@@ -1,13 +1,18 @@
 import StyledPeople from "../styles/StyledPeople";
 import StyledSearchBarCard from "../styles/StyledSearchBarCard";
 import SearchBar from "./SearchBar";
+import Loading from "./Loading";
 import useNavMenu from "../hooks/useNavMenu";
-import { useLoaderData, useOutletContext } from "react-router-dom";
+import { useOutletContext } from "react-router-dom";
 import Person from "./Person";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { database } from "../data/constants";
 
 const People = () => {
   useNavMenu();
-  const data = useLoaderData();
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState({ user: { friends: [] }, strangers: [] });
   const friends = data.user.friends;
   const strangers = data.strangers;
 
@@ -18,35 +23,54 @@ const People = () => {
 
   const totalUsers = [...filteredFriends, ...filteredStrangers];
 
+  useEffect(() => {
+    async function getUsers() {
+      const { data } = await axios({
+        url: `${database}/people`,
+        headers: { Authorization: `Bearer ${localStorage.token}` },
+      });
+
+      setData(data);
+      setIsLoading(false);
+    }
+    getUsers();
+  });
+
   return (
     <>
-      <StyledSearchBarCard>
-        <SearchBar setQuery={setQuery} query={query} />
-      </StyledSearchBarCard>
-      <StyledPeople>
-        {totalUsers.length > 0 ? (
-          <>
-            {filteredFriends.length > 0 && (
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <>
+          <StyledSearchBarCard>
+            <SearchBar setQuery={setQuery} query={query} />
+          </StyledSearchBarCard>
+          <StyledPeople>
+            {totalUsers.length > 0 ? (
               <>
-                <h3>Friends</h3>
-                {filteredFriends.map((friend) => {
-                  return <Person person={friend} key={friend._id} />;
-                })}
+                {filteredFriends.length > 0 && (
+                  <>
+                    <h3>Friends</h3>
+                    {filteredFriends.map((friend) => {
+                      return <Person person={friend} key={friend._id} />;
+                    })}
+                  </>
+                )}
+                {filteredStrangers.length > 0 && (
+                  <>
+                    <h3>People</h3>
+                    {filteredStrangers.map((stranger) => {
+                      return <Person person={stranger} key={stranger._id} />;
+                    })}
+                  </>
+                )}
               </>
+            ) : (
+              <div>No users found</div>
             )}
-            {filteredStrangers.length > 0 && (
-              <>
-                <h3>People</h3>
-                {filteredStrangers.map((stranger) => {
-                  return <Person person={stranger} key={stranger._id} />;
-                })}
-              </>
-            )}
-          </>
-        ) : (
-          <div>No users found</div>
-        )}
-      </StyledPeople>
+          </StyledPeople>
+        </>
+      )}
     </>
   );
 };
--- src/loader/people.js
+++ src/loader/people.js
@@ -1,18 +1,11 @@
 import { redirect } from "react-router-dom";
-import axios from "axios";
-import { database } from "../data/constants";
 
 const peopleLoader = async () => {
   if (!localStorage.token) {
     return redirect("/login");
   }
 
-  const { data } = await axios({
-    url: `${database}/people`,
-    headers: { Authorization: `Bearer ${localStorage.token}` },
-  });
-
-  return data;
+  return null;
 };
 
 export default peopleLoader;
