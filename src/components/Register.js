c279358f8dd39d9e0f79eca63ec32fef8c0cd2f8
MichaelShawUK <MichaelShawUK@gmail.com>
2023-06-08 11:25:03 +0100

    Refactor Register component to show loading spinner on submission

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,50 +1,107 @@
 import StyledButton from "../styles/StyledButton";
-import StyledForm from "../styles/StyledForm";
 import StyledLogin from "../styles/StyledLogin";
 import RegisterSvg from "../assets/svg/RegisterSvg";
-import { Link, useActionData } from "react-router-dom";
+import { Link, useFetcher } from "react-router-dom";
+import { useState, useEffect } from "react";
+import Loading from "./Loading";
 
 const Register = () => {
-  const errors = useActionData();
+  const [errors, setErrors] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [form, setForm] = useState({
+    username: "",
+    firstName: "",
+    lastName: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const fetcher = useFetcher();
+
+  useEffect(() => {
+    if (fetcher.state === "submitting") {
+      setIsLoading(true);
+      const formData = Object.fromEntries(fetcher.formData);
+      setForm((f) => {
+        return {
+          ...f,
+          ...formData,
+        };
+      });
+    }
+    if (fetcher.state === "idle") {
+      setIsLoading(false);
+      if (fetcher.data) {
+        setErrors(fetcher.data);
+      }
+    }
+  }, [fetcher]);
 
   return (
-    <StyledLogin>
-      <StyledForm action="/register" method="POST">
-        <h2>Get Started</h2>
-        <label>
-          <p className="error">{errors?.username}</p>
-          Username
-          <input type="text" name="username" autoFocus></input>
-        </label>
-        <label>
-          <p className="error">{errors?.firstName}</p>
-          First Name
-          <input type="text" name="firstName"></input>
-        </label>
-        <label>
-          <p className="error">{errors?.lastName}</p>
-          Last Name
-          <input type="text" name="lastName"></input>
-        </label>
-        <label>
-          <p className="error">{errors?.password}</p>
-          Password
-          <input type="password" name="password"></input>
-        </label>
-        <label>
-          Confirm Password
-          <input type="password" name="confirmPassword"></input>
-        </label>
-        <div>
-          <p className="error">{errors?.database}</p>
-          <StyledButton type="submit">Register</StyledButton>
-        </div>
-        <p>
-          Already have an account? <Link to="/login">Log In</Link>{" "}
-        </p>
-      </StyledForm>
-      <RegisterSvg />
-    </StyledLogin>
+    <>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <StyledLogin>
+          <fetcher.Form method="POST">
+            <h2>Get Started</h2>
+            <label>
+              <p className="error">{errors?.username}</p>
+              Username
+              <input
+                type="text"
+                name="username"
+                autoFocus
+                defaultValue={form.username}
+              ></input>
+            </label>
+            <label>
+              <p className="error">{errors?.firstName}</p>
+              First Name
+              <input
+                type="text"
+                name="firstName"
+                defaultValue={form.firstName}
+              ></input>
+            </label>
+            <label>
+              <p className="error">{errors?.lastName}</p>
+              Last Name
+              <input
+                type="text"
+                name="lastName"
+                defaultValue={form.lastName}
+              ></input>
+            </label>
+            <label>
+              <p className="error">{errors?.password}</p>
+              Password
+              <input
+                type="password"
+                name="password"
+                defaultValue={form.password}
+              ></input>
+            </label>
+            <label>
+              Confirm Password
+              <input
+                type="password"
+                name="confirmPassword"
+                defaultValue={form.confirmPassword}
+              ></input>
+            </label>
+            <div>
+              <p className="error">{errors?.database}</p>
+              <StyledButton type="submit">Register</StyledButton>
+            </div>
+            <p>
+              Already have an account? <Link to="/login">Log In</Link>{" "}
+            </p>
+          </fetcher.Form>
+          <RegisterSvg />
+        </StyledLogin>
+      )}
+    </>
   );
 };
 
