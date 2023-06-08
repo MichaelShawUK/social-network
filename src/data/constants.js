46f1521d975b3cecb83c073a404e04ff557c8475
MichaelShawUK <MichaelShawUK@gmail.com>
2023-06-08 13:33:20 +0100

    Update production back-end URL

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,4 +1,4 @@
-const productionDb = "http://odinbook-api-production-9b7b.up.railway.app";
+const productionDb = "https://odinbook-api-production-9b7b.up.railway.app";
 const devDb = "http://localhost:3000";
 
 const database = productionDb || devDb;
