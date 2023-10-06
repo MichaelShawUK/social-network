6e29644d76dc3c8e02626a2fecc8eaa348e3748e
MichaelShawUK <MichaelShawUK@gmail.com>
2023-10-06 14:09:43 +0100

    Remove no-cors mode

--- src/components/Test.js
+++ src/components/Test.js
@@ -2,10 +2,7 @@ function Test() {
   async function connectToDb() {
     console.log("connecting to db");
     const response = await fetch(
-      "https://odinbook-api-production-9b7b.up.railway.app/test",
-      {
-        mode: "no-cors",
-      }
+      "https://odinbook-api-production-9b7b.up.railway.app/test"
     );
     const data = await response.json();
     console.log(data);
